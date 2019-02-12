90dcf20f10715426cf293db2ad12662c68eeec25
allannnguyen <allan.n.nguyen@gmail.com>
2019-02-12 11:53:08 -0500

    Added Player, ConfigurationViewModel, ConfigurationActivity

---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Player.java
@@ -0,0 +1,4 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
+
+public class Player {
+}
---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/viewmodels/ConfigurationViewModel.java
@@ -0,0 +1,4 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp.viewmodels;
+
+public class ConfigurationViewModel {
+}
