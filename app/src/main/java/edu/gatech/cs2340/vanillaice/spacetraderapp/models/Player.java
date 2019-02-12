e2a7255f658f53cd224bcd8b4fc32e1938c8ee16
allannnguyen <allan.n.nguyen@gmail.com>
2019-02-12 12:37:35 -0500

    Added constructor, getter methods for username, skill points, and credits.

--- app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Player.java
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Player.java
@@ -1,4 +1,38 @@
 package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
 
 public class Player {
+    private String name;
+    private int pilot;
+    private int fighter;
+    private int trader;
+    private int engineer;
+    private int credits;
+
+    public Player(String name, int pilot, int fighter, int trader, int engineer) {
+        this.name = name;
+        this.pilot = pilot;
+        this.fighter = fighter;
+        this.trader = trader;
+        this.engineer = engineer;
+        this.credits = 1000;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getPilot(){
+        return pilot;
+    }
+    public int getFighter(){
+        return fighter;
+    }
+    public int getTrader(){
+        return trader;
+    }
+    public int getEngineer(){
+        return engineer;
+    }
+    public int getCredits() {
+        return credits;
+    }
 }
