f448573f55f9e9c7316ae71ff2f0bc0e8c7cd6b1
ckim311 <ckim311@gatech.edu>
2019-02-12 12:36:54 -0500

    Created Difficulty and Ships enums

---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Difficulty.java
@@ -0,0 +1,9 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
+
+public enum Difficulty {
+    BEGINNER,
+    EASY,
+    NORMAL,
+    HARD,
+    IMPOSSIBLE;
+}
---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Ships.java
@@ -0,0 +1,5 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
+
+public enum Ships {
+    GNAT;
+}
