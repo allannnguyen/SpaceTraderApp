1ba25bcf26fca8da563bc5f552ee3c474b9d1f70
Cobalts1 <watkins207@gmail.com>
2019-02-14 11:41:27 -0500

    Added javadoc implementation for difficulty.java and ships.java

--- app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Player.java
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Player.java
@@ -1,5 +1,12 @@
 package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
 
+import android.support.annotation.NonNull;
+
+/**
+ * Represents a player for the game.
+ * A player has a name, four skills with sixteen skill points allocated across them,
+ * a difficulty setting for the game, their number of credits, and a ship they own.
+ */
 public class Player {
     private String name;
     private int pilot;
@@ -10,6 +17,15 @@ public class Player {
     private Difficulty difficulty;
     private Ships ship;
 
+    /**
+     * Constructs the player based on various parameters.
+     * @param name the player's name
+     * @param difficulty the selected difficulty for the game
+     * @param pilot number of skill points allocated to the pilot skill
+     * @param fighter number of skill points allocated to the fighter skill
+     * @param trader number of skill points allocated to the trader skill
+     * @param engineer number of skill points allocated to the engineer skill
+     */
     public Player(String name, Difficulty difficulty, int pilot, int fighter, int trader, int engineer) {
         this.name = name;
         this.difficulty = difficulty;
@@ -21,25 +37,75 @@ public class Player {
         this.ship = Ships.GNAT;
     }
 
+    /**
+     * Gets the name of the player
+     * @return this player's name
+     */
     public String getName() {
         return name;
     }
-    public int getPilot(){ return pilot; }
+
+    /**
+     * Gets the amount of skill points allocated to the pilot skill
+     * @return player's pilot skill points
+     */
+    public int getPilot(){
+        return pilot;
+    }
+
+    /**
+     * Gets the amount of skill points allocated to the fighter skill
+     * @return player's fighter skill points
+     */
     public int getFighter(){
         return fighter;
     }
+
+    /**
+     * Gets the amount of skill points allocated to the trader skill
+     * @return player's trader skill points
+     */
     public int getTrader(){
         return trader;
     }
+
+    /**
+     * Gets the amount of skill points allocated to the engineer skill
+     * @return player's engineer skill points
+     */
     public int getEngineer(){
         return engineer;
     }
+
+    /**
+     * Gets the amount of credits the player has
+     * @return player's number of credits
+     */
     public int getCredits() {
         return credits;
     }
-    public Difficulty getDifficulty(){ return difficulty;}
-    public Ships getShip(){ return ship;}
 
+    /**
+     * Gets the difficulty the player selected
+     * @return player's difficulty setting
+     */
+    public Difficulty getDifficulty(){
+        return difficulty;
+    }
+
+    /**
+     * Gets the ship the player owns
+     * @return player's current ship
+     */
+    public Ships getShip(){
+        return ship;
+    }
+
+    /**
+     * Prints the player's information to the logs
+     * @return a string that lists the player's parameters
+     */
+    @NonNull
     public String toString() {
         return "|Commander Name: " + name + "|Difficulty: " + difficulty.toString()
                 + "|Ship: " + ship.toString() + "| SKILLS -- Pilot: "
