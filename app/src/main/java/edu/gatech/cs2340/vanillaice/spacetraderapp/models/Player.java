037b5e8bc67668dd6841d5c9401872028dc6f85a
allannnguyen <allan.n.nguyen@gmail.com>
2019-02-14 10:31:04 -0500

    Added difficulty and ships

--- app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Player.java
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Player.java
@@ -7,22 +7,24 @@ public class Player {
     private int trader;
     private int engineer;
     private int credits;
+    private Difficulty difficulty;
+    private Ships ship;
 
-    public Player(String name, int pilot, int fighter, int trader, int engineer) {
+    public Player(String name, Difficulty difficulty, int pilot, int fighter, int trader, int engineer) {
         this.name = name;
+        this.difficulty = difficulty;
         this.pilot = pilot;
         this.fighter = fighter;
         this.trader = trader;
         this.engineer = engineer;
         this.credits = 1000;
+        this.ship = Ships.GNAT;
     }
 
     public String getName() {
         return name;
     }
-    public int getPilot(){
-        return pilot;
-    }
+    public int getPilot(){ return pilot; }
     public int getFighter(){
         return fighter;
     }
@@ -35,4 +37,6 @@ public class Player {
     public int getCredits() {
         return credits;
     }
+    public Difficulty getDifficulty(){ return difficulty;}
+    public Ships getShip(){ return ship;}
 }
