5cc62565f354be3ed4679e791e8579f7ccad7ebe
Shakeeb Shams <shakeebshams@gmail.com>
2019-02-21 11:40:23 -0500

    Added transition from main to gameActivity

---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/views/GameActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.vanillaice.spacetraderapp.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
