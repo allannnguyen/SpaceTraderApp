5f10bd37d3071970996bd2d505e5a542a9ccbd89
ckim311 <ckim311@gatech.edu>
2019-02-21 10:13:06 -0500

    Added enums for Technology and Resource levels

---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Resource.java
@@ -0,0 +1,17 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
+
+public enum Resource {
+    NOSPECIALRESOURCES,
+    MINERALRICH,
+    MINERALPOOR,
+    DESERT,
+    LOTSOFWATER,
+    RICHSOIL,
+    POORSOIL,
+    RICHFAUNA,
+    LIFELESS,
+    WEIRDMUSHROOMS,
+    LOTSOFHERBS,
+    ARTISTIC,
+    WARLIKE;
+}
---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Technology.java
@@ -0,0 +1,12 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
+
+public enum Technology {
+    PREAGRICULTURAL,
+    AGRICULTURAL,
+    MEDIEVAL,
+    RENAISSANCE,
+    EARLYINDUSTRIAL,
+    INDUSTRIAL,
+    POSTINDUSTRIAL,
+    HITECH;
+}
