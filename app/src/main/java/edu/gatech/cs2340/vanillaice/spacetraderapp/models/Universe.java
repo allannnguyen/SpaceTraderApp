640b7c64c5990ce878a5e2f6706a5b382b5d7c8d
Cobalts1 <watkins207@gmail.com>
2019-02-21 11:56:16 -0500

    Modified the names of the planets and added javadoc implementation

--- app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Universe.java
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Universe.java
@@ -1,57 +1,63 @@
 package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
+/**
+ * Represents the Universe the game takes place in
+ * A universe has a list of randomly selected planets from all precreated options
+ */
 public class Universe {
-    Planet A = new Planet("A", 0, 0, Technology.PREAGRICULTURAL, Resource.WARLIKE); //ANARCHY
-    Planet B = new Planet("B", 0, 0, Technology.AGRICULTURAL, Resource.RICHSOIL); //DICTATORSHIP
-    Planet C = new Planet("C", 0, 0, Technology.MEDIEVAL, Resource.RICHFAUNA); //FEUDALSTATE
-    Planet D = new Planet("D", 0, 0, Technology.RENAISSANCE, Resource.ARTISTIC); //DEMOCRACY
-    Planet E = new Planet("E", 0, 0, Technology.EARLYINDUSTRIAL, Resource.WEIRDMUSHROOMS); //MONARCHY
-    Planet F = new Planet("F", 0, 0, Technology.INDUSTRIAL, Resource.MINERALRICH); //FACISTSTATE
-    Planet G = new Planet("G", 0, 0, Technology.POSTINDUSTRIAL, Resource.MINERALPOOR); //DICTATORSHIP
-    Planet H = new Planet("H", 0, 0, Technology.HITECH, Resource.LOTSOFWATER); //PACIFISTSTATE
-    Planet I = new Planet("I", 0, 0, Technology.PREAGRICULTURAL, Resource.LIFELESS); //DEMOCRACY
-    Planet J = new Planet("J", 0, 0, Technology.AGRICULTURAL, Resource.LOTSOFHERBS); //CONFEDERACY
-    Planet K = new Planet("K", 0, 0, Technology.MEDIEVAL, Resource.DESERT); //COMMUNISTSTATE
-    Planet L = new Planet("L", 0, 0, Technology.RENAISSANCE, Resource.NOSPECIALRESOURCES); //FACISTSTATE
-    Planet M = new Planet("M", 0, 0, Technology.EARLYINDUSTRIAL, Resource.MINERALPOOR); //SOCIALISTSTATE
-    Planet N = new Planet("N", 0, 0, Technology.INDUSTRIAL, Resource.POORSOIL); //DEMOCRACY
-    Planet O = new Planet("O", 0, 0, Technology.POSTINDUSTRIAL, Resource.NOSPECIALRESOURCES); //DICTATORSHIP
-    Planet P = new Planet("P", 0, 0, Technology.HITECH, Resource.MINERALRICH); //CYBERNETICSTATE
-    Planet Q = new Planet("Q", 0, 0, Technology.HITECH, Resource.WARLIKE); //ANARCHY
-    Planet R = new Planet("R", 0, 0, Technology.POSTINDUSTRIAL, Resource.ARTISTIC); //THEOCRACY
-    Planet S = new Planet("S", 0, 0, Technology.INDUSTRIAL, Resource.LOTSOFWATER); //CAPITALISTSTATE
-    Planet T = new Planet("T", 0, 0, Technology.MEDIEVAL, Resource.WEIRDMUSHROOMS); //STATEOFSTAORI
-    Planet U = new Planet("U", 0, 0, Technology.HITECH, Resource.NOSPECIALRESOURCES); //TECHNOCRACY
-    Planet V = new Planet("V", 0, 0, Technology.POSTINDUSTRIAL, Resource.RICHSOIL); //FEUDALSTATE
+    Planet CharlesTheRed = new Planet("Charles The Red", 0, 0, Technology.PREAGRICULTURAL, Resource.WARLIKE); //ANARCHY
+    Planet Wrangler = new Planet("Wrangler", 0, 0, Technology.AGRICULTURAL, Resource.RICHSOIL); //DICTATORSHIP
+    Planet Yojimbo = new Planet("Yojimbo", 0, 0, Technology.MEDIEVAL, Resource.RICHFAUNA); //FEUDALSTATE
+    Planet Cobaltea = new Planet("Cobaltea", 0, 0, Technology.RENAISSANCE, Resource.ARTISTIC); //DEMOCRACY
+    Planet SuperFunTimeLand = new Planet("Super-Fun-Time-Land", 0, 0, Technology.EARLYINDUSTRIAL, Resource.WEIRDMUSHROOMS); //MONARCHY
+    Planet StreronR2Y = new Planet("Streron R2Y", 0, 0, Technology.INDUSTRIAL, Resource.MINERALRICH); //FACISTSTATE
+    Planet Cendicarro = new Planet("Cendicarro", 0, 0, Technology.POSTINDUSTRIAL, Resource.MINERALPOOR); //DICTATORSHIP
+    Planet Nunvolla = new Planet("Nunvolla", 0, 0, Technology.HITECH, Resource.LOTSOFWATER); //PACIFISTSTATE
+    Planet Laewei = new Planet("Laewei", 0, 0, Technology.PREAGRICULTURAL, Resource.LIFELESS); //DEMOCRACY
+    Planet Huthone = new Planet("Huthone", 0, 0, Technology.AGRICULTURAL, Resource.LOTSOFHERBS); //CONFEDERACY
+    Planet Besars = new Planet("Besars", 0, 0, Technology.MEDIEVAL, Resource.DESERT); //COMMUNISTSTATE
+    Planet Gninda9D3 = new Planet("Gninda 9D3", 0, 0, Technology.RENAISSANCE, Resource.NOSPECIALRESOURCES); //FACISTSTATE
+    Planet Cusurn = new Planet("Cusurn", 0, 0, Technology.EARLYINDUSTRIAL, Resource.MINERALPOOR); //SOCIALISTSTATE
+    Planet Choivis = new Planet("Choivis", 0, 0, Technology.INDUSTRIAL, Resource.POORSOIL); //DEMOCRACY
+    Planet GaraDPX = new Planet("Gara DPX", 0, 0, Technology.POSTINDUSTRIAL, Resource.NOSPECIALRESOURCES); //DICTATORSHIP
+    Planet Broanus = new Planet("Broanus", 0, 0, Technology.HITECH, Resource.MINERALRICH); //CYBERNETICSTATE
+    Planet Gunkilia = new Planet("Gunkilia", 0, 0, Technology.HITECH, Resource.WARLIKE); //ANARCHY
+    Planet Bralatune = new Planet("Bralatune", 0, 0, Technology.POSTINDUSTRIAL, Resource.ARTISTIC); //THEOCRACY
+    Planet Ebbichi = new Planet("Ebbichi", 0, 0, Technology.INDUSTRIAL, Resource.LOTSOFWATER); //CAPITALISTSTATE
+    Planet Xamecarro = new Planet("Xamecarro", 0, 0, Technology.MEDIEVAL, Resource.WEIRDMUSHROOMS); //STATEOFSTAORI
+    Planet Lichetune = new Planet("Lichetune", 0, 0, Technology.HITECH, Resource.NOSPECIALRESOURCES); //TECHNOCRACY
+    Planet Brichi8X5 = new Planet("Brichi8X5", 0, 0, Technology.POSTINDUSTRIAL, Resource.RICHSOIL); //FEUDALSTATE
 
+    /**
+     * Selects the planets for this game randomly from all options
+     */
     public Universe() {
         ArrayList<Planet> planets = new ArrayList<Planet>();
-        planets.add(A);
-        planets.add(B);
-        planets.add(C);
-        planets.add(D);
-        planets.add(E);
-        planets.add(F);
-        planets.add(G);
-        planets.add(H);
-        planets.add(I);
-        planets.add(J);
-        planets.add(K);
-        planets.add(L);
-        planets.add(M);
-        planets.add(N);
-        planets.add(O);
-        planets.add(P);
-        planets.add(Q);
-        planets.add(R);
-        planets.add(S);
-        planets.add(T);
-        planets.add(U);
-        planets.add(V);
+        planets.add(CharlesTheRed);
+        planets.add(Wrangler);
+        planets.add(Yojimbo);
+        planets.add(Cobaltea);
+        planets.add(SuperFunTimeLand);
+        planets.add(StreronR2Y);
+        planets.add(Cendicarro);
+        planets.add(Nunvolla);
+        planets.add(Laewei);
+        planets.add(Huthone);
+        planets.add(Besars);
+        planets.add(Gninda9D3);
+        planets.add(Cusurn);
+        planets.add(Choivis);
+        planets.add(GaraDPX);
+        planets.add(Broanus);
+        planets.add(Gunkilia);
+        planets.add(Bralatune);
+        planets.add(Ebbichi);
+        planets.add(Xamecarro);
+        planets.add(Lichetune);
+        planets.add(Brichi8X5);
         Random nextInt = new Random();
 
     }
