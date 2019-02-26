9b5a854aaaf4f4cfaa6bcac2f95ae30f81b3e090
Chilshifter <ben10chuang@gmail.com>
2019-02-26 12:30:40 -0500

    Added goodsBuy, goodsSell, goodsPrice

--- app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Market.java
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Market.java
@@ -1,16 +1,38 @@
 package edu.gatech.cs2340.vanillaice.spacetraderapp.models;
 
+import java.util.HashMap;
+
 public class Market {
     private Technology techLevel;
     private Resource resourceLevel;
-
+    private HashMap<Good, Boolean> goodsBuy;
+    private HashMap<Good, Boolean> goodsSell;
+    private HashMap<Good, Integer> goodsPrice;
 
     public Market(Technology techLevel, Resource resourceLevel) {
         this.techLevel = techLevel;
         this.resourceLevel = resourceLevel;
+
+        for (Good good : Good.values()) {
+            // If they can produce, they can buy and sell
+            if (techLevel.ordinal() >= good.getMtlp()) {
+                goodsBuy.put(good, true);
+                goodsSell.put(good, true);
+
+            // if they can use, they can buy but not sell
+            } else if (techLevel.ordinal() >= good.getMtlu()) {
+                goodsBuy.put(good, true);
+                goodsSell.put(good, false);
+            }
+
+            goodsPrice.put(good, getPrice(good));
+        }
     }
 
-    public int getPrice() {
-
+    public int getPrice(Good good) {
+        // base price * (IPL * (PTL - MTLP)) + variance
+        return good.getBasePrice() * (good.getIpl() * (techLevel.ordinal() - good.getMtlp())) + good.getVar();
     }
+
+    
 }
