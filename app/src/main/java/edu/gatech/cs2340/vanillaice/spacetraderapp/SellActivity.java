fbb9835f42dea101ee1777c5e90c2779cdcab97c
Shakeeb Shams <shakeebshams@gmail.com>
2019-02-28 12:27:27 -0500

    MarketView created with hardcoded functionality

---
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/SellActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.cs2340.vanillaice.spacetraderapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SellActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell);
+    }
+}
