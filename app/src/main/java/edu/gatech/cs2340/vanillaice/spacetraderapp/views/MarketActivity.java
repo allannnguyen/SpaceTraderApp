ee733d342e4c7c832d4a1305875a53dac0077f58
Shakeeb Shams <shakeebshams@gmail.com>
2019-03-01 16:16:44 -0500

    updated folder located for 3 views

--- app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/views/MarketActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/views/MarketActivity.java
@@ -1,12 +1,12 @@
-package edu.gatech.cs2340.vanillaice.spacetraderapp;
+package edu.gatech.cs2340.vanillaice.spacetraderapp.views;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import edu.gatech.cs2340.vanillaice.spacetraderapp.R;
 
 public class MarketActivity extends AppCompatActivity {
     private Button buyButton;
