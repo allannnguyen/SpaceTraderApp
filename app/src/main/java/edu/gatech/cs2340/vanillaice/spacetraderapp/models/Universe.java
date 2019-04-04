8f38a8ecc62544bf67bdf5c74fd5b5391d64038b
Cobalts1 <watkins207@gmail.com>
2019-04-04 11:19:16 -0400

    Attempted to fix encapsulation warning in Universe.java again

--- app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Universe.java
+++ app/src/main/java/edu/gatech/cs2340/vanillaice/spacetraderapp/models/Universe.java
@@ -119,7 +119,7 @@ public class Universe implements Serializable {
      * Gets the list of planets in the universe
      * @return a list of planets contained within the universe
      */
-    public final List<Planet> getPlanets() {
+    private List<Planet> getPlanets() {
         return tenplanets;
     }
 }
